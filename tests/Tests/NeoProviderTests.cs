9f829de7f183ec21988ede722c9833aa7e4241b6
some-dev <some-dev@gmail.com>
2019-07-04 00:36:59 +0300

    fix neo tests

--- tests/Tests/NeoProviderTests.cs
+++ tests/Tests/NeoProviderTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Lykke.Tools.BlockchainBalancesReport.Blockchains.Neo;
 using Lykke.Tools.BlockchainBalancesReport.Blockchains;
@@ -15,9 +14,9 @@ namespace Tests
         {
             var balanceProvider = new NeoBalanceProvider("https://neoscan.io/api/main_net/v1/");
 
-            var neoAsset = new Asset("Neo", "c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b", "ac2e579f-187b-4429-8d60-bea6e4f65f76");
-            var gasAsset = new Asset("Gas", "602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7", "f1ccf1dd-9008-4999-adc8-2cb587717083");
-            var seasAsset = new Asset("de7be47c4c93f1483a0a3fff556a885a68413d97", "de7be47c4c93f1483a0a3fff556a885a68413d97", null);
+            var neoAsset = new Asset("NEO", "c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b", "ac2e579f-187b-4429-8d60-bea6e4f65f76");
+            var gasAsset = new Asset("GAS", "602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7", "f1ccf1dd-9008-4999-adc8-2cb587717083");
+            var seasAsset = new Asset("SEAS", "de7be47c4c93f1483a0a3fff556a885a68413d97", null);
 
             var expectations = new List<(string address, DateTime dateTime, IReadOnlyDictionary<Asset, decimal> result)>
             {
