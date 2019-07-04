d47d5e4f4c785698f048555b034b85fa3762a526
some-dev <some-dev@gmail.com>
2019-07-04 19:57:46 +0300

    WIP

---
+++ tests/Tests/SteemProviderTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lykke.Tools.BlockchainBalancesReport.Blockchains;
+using Lykke.Tools.BlockchainBalancesReport.Blockchains.Steem;
+using Xunit;
+
+namespace Tests
+{
+    public class SteemProviderTests
+    {
+        [Fact]
+        public async Task CanCalculateBalanceAtPointOfTime()
+        {
+            var balanceProvider = new SteemBalanceProvider("https://api.steemit.com/");
+
+            var asset = new Asset("STEEM", "STEEM", "72da9464-49d0-4f95-983d-635c04e39f3c");
+            
+            var expectations = new List<(string address, DateTime dateTime, IReadOnlyDictionary<Asset, decimal> result)>
+            {
+                ("lykke-exchange", DateTime.Parse("2019-04-07T19:00:00+0000"), new Dictionary<Asset, decimal>
+                {
+                    {asset, 2751.324m }
+                })
+            };
+
+            foreach (var assert in expectations)
+            {
+                var result = await balanceProvider.GetBalancesAsync(assert.address, assert.dateTime);
+
+                Assert.Equal(assert.result[asset], result[asset]);
+            }
+        }
+    }
+}
