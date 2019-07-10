bcebf2425ba488768fd51506d44a0c4612ae7438
some-dev <some-dev@gmail.com>
2019-07-10 10:57:41 +0300

    WIP

--- tests/Tests/SteemProviderTests.cs
+++ tests/Tests/SteemProviderTests.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Lykke.Tools.BlockchainBalancesReport.Blockchains;
 using Lykke.Tools.BlockchainBalancesReport.Blockchains.Steem;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace Tests
@@ -30,6 +33,43 @@ namespace Tests
 
                 Assert.Equal(assert.result[asset], result[asset]);
             }
+        }
+
+        [Fact]
+        public async Task TaskCanDeserializeSteemitResponse()
+        {
+            var respExample =await File.ReadAllTextAsync("SteemetResp.json");
+
+            var typed = Deserialize(respExample).ToList();
+
+            Assert.NotEmpty(typed);
+
+            foreach (var item in typed)
+            {
+                Assert.False(string.IsNullOrEmpty(item.txId));
+            }
+        }
+
+        private IEnumerable<(string txId, DateTime from)> Deserialize(string source)
+        {
+            var model = JsonConvert.DeserializeObject<SteemetResp>(source);
+            var txs = model.Result.Select(p => p[1]).ToList();
+
+            foreach (var tx  in txs)
+            {
+                var t = JsonConvert.SerializeObject(tx.op);
+                var t2 = JsonConvert.DeserializeObject<dynamic[]>(t);
+                if (t2[0] == "transfer")
+                {
+
+                    yield return (tx.trx_id, DateTime.Parse(tx.timestamp.ToString()));
+                }
+            }
+        }
+
+        public class SteemetResp
+        {
+            public dynamic[][] Result { get; set; }
         }
     }
 }
