3ed582c3ea66ad86f2cfc6bbf35ce33aa1f988f8
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2019-07-12 02:49:58 +0300

    Just copied existing sources to the template job

---
+++ src/Lykke.Job.BlockchainBalancesReport/Settings/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿using Lykke.Job.BlockchainBalancesReport.Settings.JobSettings;
+using Lykke.Sdk.Settings;
+
+namespace Lykke.Job.BlockchainBalancesReport.Settings
+{
+    public class AppSettings : BaseAppSettings
+    {
+        public BlockchainBalancesReportJobSettings BlockchainBalancesReportJob { get; set; }
+    }
+}
